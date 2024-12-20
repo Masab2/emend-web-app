bd4f48da7633b62f23d47fc7502f2598dcd5e364
faizaashraf6093 <faizaashraf0786@gmail.com>
2024-12-21 03:17:36 +0500

    UI fixes

--- lib/views/TemplateView/template_view.dart
+++ lib/views/TemplateView/template_view.dart
@@ -1,3 +1,4 @@
+import 'package:emend_web_app/config/Widgets/templateWidgets/template_header.dart';
 import 'package:emend_web_app/config/assets/image_asset.dart';
 import 'package:emend_web_app/config/color/app_color.dart';
 import 'package:emend_web_app/config/extenshions/extenshion.dart';
@@ -23,84 +24,120 @@ class TemplateView extends StatelessWidget {
     return Container(
       color: AppColor.viewsBackgroundColor,
       height: context.mh,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: context.mw * 0.02),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Header
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Email Templates",
-                    style: GoogleFonts.barlow(
-                      fontSize: context.mh * 0.028,
-                      fontWeight: FontWeight.w500,
+      width: context.mw,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: context.mw * 0.02),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header
+            const TemplateHeaderWidget(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Email Templates",
+                  style: GoogleFonts.barlow(
+                    fontSize: context.mh * 0.028,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(UnLayerEditor.route);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Get.toNamed(UnLayerEditor.route);
-                    },
-                    child: const Text('Creates Template'),
-                  )
-                ],
-              ),
-              // Template Cards
-              0.02.ph(context),
-              Wrap(
-                spacing: 20,
-                runSpacing: 30,
-                children: templates.map((template) {
-                  return Container(
-                    height: context.mh * 0.45,
-                    width: context.mw * 0.15,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                        image: AssetImage(template['image']!),
-                        fit: BoxFit.cover,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    child: Text(
+                      "Create New",
+                      style: GoogleFonts.barlow(
+                        fontSize: context.mh * 0.020,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        // Open Template Button
-                        Container(
-                          margin: const EdgeInsets.only(bottom: 10),
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: AppColor.primaryColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 8),
-                              child: Text(
-                                "Open Template",
-                                style: GoogleFonts.barlow(
-                                  fontSize: context.mh * 0.020,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                  ),
+                ),
+              ],
+            ),
+            // Template Cards
+            0.02.ph(context),
+            SizedBox(
+              height: context.mh * 0.8,
+              width: context.mw * 0.8,
+              child: SingleChildScrollView(
+                  child: TemplateGrid(templates: templates)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TemplateGrid extends StatelessWidget {
+  const TemplateGrid({
+    super.key,
+    required this.templates,
+  });
+
+  final List<Map<String, String>> templates;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 20,
+      runSpacing: 30,
+      children: templates.map((template) {
+        return Container(
+          height: context.mh * 0.45,
+          width: context.mw * 0.15,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(template['image']!),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // Open Template Button
+              Container(
+                margin: const EdgeInsets.only(bottom: 10),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  );
-                }).toList(),
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    child: Text(
+                      "Email Template 1",
+                      style: GoogleFonts.barlow(
+                        fontSize: context.mh * 0.020,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
-        ),
-      ),
+        );
+      }).toList(),
     );
   }
 }
